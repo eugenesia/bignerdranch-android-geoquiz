efb93ddd90b135d64dd5013ff9dd6d0930dee6ba
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-05 08:57:01 +0100

    Set listener for True Button lst 1.9

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class QuizActivity extends ActionBarActivity {
@@ -17,6 +18,13 @@ public class QuizActivity extends ActionBarActivity {
     setContentView(R.layout.activity_quiz);
 
     mTrueButton = (Button) findViewById(R.id.true_button);
+    mTrueButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // does nothing yet, but soon!
+      }
+    });
+
     mFalseButton = (Button) findViewById(R.id.false_button);
   }
 
