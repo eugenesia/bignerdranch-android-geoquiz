1e9db2d509678aacc3da884a566e7fd314b1ebfd
Eugene Sia <eugene@eugenesia.co.uk>
2015-10-05 13:54:24 +0100

    Make toasts lst 1.12

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
 
@@ -21,7 +22,11 @@ public class QuizActivity extends ActionBarActivity {
     mTrueButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // does nothing yet, but soon!
+
+        // Show a Toast that says "Incorrect!".
+        Toast.makeText(QuizActivity.this,
+          R.string.incorrect_toast,
+          Toast.LENGTH_SHORT).show();
       }
     });
 
@@ -29,7 +34,11 @@ public class QuizActivity extends ActionBarActivity {
     mFalseButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // does nothing yet, but soon!
+
+        // Show a Toast that says "Correct!".
+        Toast.makeText(QuizActivity.this,
+          R.string.correct_toast,
+          Toast.LENGTH_SHORT).show();
       }
     });
   }
